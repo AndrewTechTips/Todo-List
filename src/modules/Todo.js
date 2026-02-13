9eabaabe91825b646115dde9d5edea0b67f5ae99
AndrewTechTips <andreww.steel69@gmail.com>
2026-02-13 19:41:30 +0200

    create todo object class in js

---
+++ src/modules/Todo.js
@@ -0,0 +1,21 @@
+export default class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.isComplete = false;
+        this.id = Date.now().toString;
+    }
+
+    toggleComplete() {
+        this.isComplete = !this.isComplete;
+    }
+
+    updateDetails(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    } 
+}
