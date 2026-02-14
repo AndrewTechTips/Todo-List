0e171dec715d4b1356b213c95c9a02fa8e27f6dd
AndrewTechTips <andreww.steel69@gmail.com>
2026-02-14 19:22:16 +0200

    add localStorage save functions

--- src/modules/todoManager.js
+++ src/modules/todoManager.js
@@ -5,14 +5,58 @@ import Todo from "./Todo.js";
 let projects = [];
 let currentProjectId = null;
 
+//Functions save and load for local storage
+function save() {
+    localStorage.setItem("myProjects", JSON.stringify(projects));
+    localStorage.setItem("currentProjectId", currentProjectId);
+}
+
+function load() {
+    const projectsData = localStorage.getItem("myProjects");
+    const activeIdData = localStorage.getItem("currentProjectId");
+
+    if(projectsData) {
+
+        const rawProjects = JSON.parse(projectsData);
+
+        projects = rawProjects.map(projData => {
+            const project = new Project(projData.name);
+            project.id = projData.id;
+
+            project.todos = projData.todos.map(todoData => {
+                const todo = new Todo(todoData.title, todoData.description, todoData.dueDate, todoData.priority);
+                todo.id = todoData.id;
+                todo.isComplete = todoData.isComplete;
+                return todo;
+            });
+            return project;
+        });
+    }
+
+    if (activeIdData) {
+        currentProjectId = activeIdData;
+    }
+
+}
+
 //Initializing the default project 
 function initialize() {
-    const defaultProject = new Project("Inbox");
-    projects.push(defaultProject);
-    currentProjectId = defaultProject.id;
+    load();
 
-    const demoTodo = new Todo("Check engine oil", "Inspect and refill if needed", "2026-02-18", "high");
-    defaultProject.addTodo(demoTodo);
+    if(projects.length === 0) {
+        const defaultProject = new Project("Inbox");
+        projects.push(defaultProject);
+        currentProjectId = defaultProject.id;
+
+        const demoTodo = new Todo("Check engine oil", "Inspect and refill if needed", "2026-02-18", "high");
+        defaultProject.addTodo(demoTodo);
+
+        save();
+    } else {
+        if(!projects.find(p => p.id === currentProjectId)) {
+            currentProjectId = projects[0].id;
+        }
+    }
 }
 
 //Return projects list via getter 
@@ -27,11 +71,13 @@ function getCurrentProject() {
 //Here we set the current project only via setter
 function setCurrentProject(projectId) {
     currentProjectId = projectId;
+    save();
 }
 
 function createProject(name) {
     const newProject = new Project(name);
     projects.push(newProject);
+    save();
     return newProject;
 }
 
@@ -45,8 +91,14 @@ function addTodoToCurrentProject(title, description, dueDate, priority) {
     if(project) {
         const newTodo = new Todo(title, description, dueDate, priority);
         project.addTodo(newTodo);
+        save();
         return newTodo;
     }
+}
+
+//Helper function for saving the data in localStorage in domController
+function saveData() {
+    save();
 }
 
 //Now we re exporting just the necessary functions, and we keep the  others private
@@ -58,6 +110,7 @@ export default {
     setCurrentProject,
     createProject,
     deleteProject,
-    addTodoToCurrentProject
+    addTodoToCurrentProject,
+    saveDatas
 };
 
